fc0b8e08687d87ba38ab50f09e1b1769eb138be6
Zachary Lieberman <181287845+zachgliebs@users.noreply.github.com>
2025-03-06 20:22:20 +0000

    now

--- backend.py
+++ backend.py
@@ -1,9 +1,9 @@
 # app.py
+from flask import Flask, request, jsonify, redirect, render_template
+import qrcode
 import os
-from flask import Flask
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
